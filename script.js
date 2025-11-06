2124785fe55c3ca045fca958d8aae67c37f8b2a1
arking290t-alt <arking290t@gmail.com>
2025-11-06 14:15:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,25 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+  const errorMsg = document.getElementById("error");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = document.getElementById("userid").value.trim();
+    const pass = document.getElementById("password").value.trim();
+    const role = document.getElementById("role").value;
+
+    if (role === "owner" && user === "pratham" && pass === "pratham0056") {
+      sessionStorage.setItem("loginRole", "owner");
+      window.location.href = "dashboard.html";
+    } else if (role === "staff") {
+      sessionStorage.setItem("loginRole", "staff");
+      window.location.href = "staff.html";
+    } else {
+      errorMsg.textContent = "Incorrect ID or Password!";
+    }
+  });
+});
